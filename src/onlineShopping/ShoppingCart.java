f47ac015220ca71014112ff3923411aa98e35465
arifc <arifc@DESKTOP-KUIAII6>
2018-11-03 12:29:26 -0500

    PhoneClass

--- src/onlineShopping/ShoppingCart.java
+++ src/onlineShopping/ShoppingCart.java
@@ -1,15 +1,29 @@
 package onlineShopping;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ShoppingCart extends JFrame implements ActionListener{
-
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private JMenu optionsMenu = new JMenu("Home");
 	private JMenu ProductMenu = new JMenu("Products");
 	private JMenuItem m1 = new JMenuItem("Phone");
@@ -20,17 +34,65 @@ public class ShoppingCart extends JFrame implements ActionListener{
 	private JMenuBar menuBar = new JMenuBar();
 	
 	
+	private JPanel productPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+	private JPanel headerPanel = new JPanel (new FlowLayout(FlowLayout.CENTER));
+	private JPanel searchPanel = new JPanel (new FlowLayout(FlowLayout.CENTER));
+	
+	private JPanel panel1 = new JPanel(new BorderLayout());
+	private JPanel panel2 = new JPanel(new BorderLayout());	
+	
+	private JTextField searchTxtF = new JTextField (20);
+	
+	private JButton searchBtn = new JButton ("Search");
+	private JButton phoneBtn = new JButton();
+	private JButton laptopBtn = new JButton();
+	private JButton acsBtn = new JButton();
+
+	
+	private JLabel companyHeaderLbl = new JLabel("AMA Online Shopping");
+
 	public ShoppingCart(String title) {
+		
 		setTitle(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,300); 
+		setSize(650,350); 
 		
-//		addComponentsToPanels();
-//		addPanelsToFrame();
+		phoneBtn.setIcon(new ImageIcon("phone.png"));
+		laptopBtn.setIcon(new ImageIcon("laptop.png"));
+		acsBtn.setIcon(new ImageIcon("acs.png"));
+		
+		addComponentsToPanels();
+		addPanelsToFrame();
 		addListeners();
 		addMenuBar();
 	}
 	
+	private void addPanelsToFrame() {
+		add(panel2);
+	}
+
+	private void addComponentsToPanels() {
+		
+		headerPanel.add(companyHeaderLbl);
+		companyHeaderLbl.setFont(new Font("AMA Online Shopping", Font.ROMAN_BASELINE,20));
+		companyHeaderLbl.setForeground(Color.BLACK);
+		
+		searchPanel.add(searchTxtF);
+		searchPanel.add(searchBtn);
+	
+		productPanel.add(phoneBtn);
+		productPanel.add(laptopBtn);
+		productPanel.add(acsBtn);
+
+		
+		panel1.add(headerPanel, BorderLayout.NORTH);
+		panel1.add(searchPanel, BorderLayout.CENTER);
+		
+		panel2.add(panel1, BorderLayout.NORTH);
+		panel2.add(productPanel, BorderLayout.CENTER);
+
+	}
+
 	private void addListeners() {	
 		
 		ProductMenu.addActionListener(this);
@@ -38,6 +100,10 @@ public class ShoppingCart extends JFrame implements ActionListener{
 		m1.addActionListener(this);
 		m2.addActionListener(this);
 		m3.addActionListener(this);
+		searchBtn.addActionListener(this);
+		phoneBtn.addActionListener(this);
+		laptopBtn.addActionListener(this);
+		acsBtn.addActionListener(this);
 	}
 	
 	private void addMenuBar() {
@@ -57,14 +123,19 @@ public class ShoppingCart extends JFrame implements ActionListener{
 		
 		setJMenuBar(menuBar);
 	}
+	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent callingObj) {
+		
+		Object obj = callingObj.getSource();
+		if (obj.equals(phoneBtn)) {
+			phoneClass phoneClass = new phoneClass();
+		}
 		
 	}
 	
 	public static void main(String[] args) {
-		ShoppingCart gui = new ShoppingCart("Online Shopping");
+		ShoppingCart gui = new ShoppingCart("Mock Online Shopping");
 		gui.setVisible(true);
 	}
 
---
+++ src/onlineShopping/phoneClass.java
@@ -0,0 +1,95 @@
+package onlineShopping;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class phoneClass extends JFrame implements ActionListener{
+	/**
+	 * 
+	 */
+	
+	private JPanel panel = new JPanel(new BorderLayout());
+	private JPanel panel2 = new JPanel(new BorderLayout());
+	
+	private JPanel headerPanel = new JPanel (new FlowLayout(FlowLayout.CENTER));
+	private JPanel phonePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+	private JPanel optionPanel = new JPanel (new FlowLayout(FlowLayout.CENTER));
+	
+	private JLabel companyHeaderLbl = new JLabel("AMA Online Shopping");
+	private JLabel pickOptionLbl = new JLabel("***PICK WHICH PHONE YOU WANT TO SHOP FROM US***", SwingConstants.CENTER);
+	
+	private JButton iPhoneBtn = new JButton();
+	private JButton samsungBtn = new JButton();
+	private JButton htcBtn = new JButton();
+	private JButton motoBtn = new JButton();
+	
+	private static final long serialVersionUID = 1L;
+	
+	public phoneClass() {
+
+		iPhoneBtn.setIcon(new ImageIcon("iphone.png"));
+		samsungBtn.setIcon(new ImageIcon("samsung.png"));
+		htcBtn.setIcon(new ImageIcon("htc.png"));
+		motoBtn.setIcon(new ImageIcon("moto.png"));
+		
+		setSize(650,350); 
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		addComponentsToPanels();
+		addPanelsToFrame();
+		addListeners();
+
+
+	}
+	
+	private void addComponentsToPanels() {
+		headerPanel.add(companyHeaderLbl);
+		companyHeaderLbl.setFont(new Font("AMA Online Shopping", Font.ROMAN_BASELINE,20));
+		companyHeaderLbl.setForeground(Color.BLACK);
+		
+		optionPanel.add(pickOptionLbl);
+		pickOptionLbl.setFont(new Font("***PICK WHICH PHONE YOU WANT TO SHOP FROM US***", Font.ITALIC,15));
+		pickOptionLbl.setForeground(Color.RED);
+		
+		phonePanel.add(iPhoneBtn);
+		phonePanel.add(samsungBtn);
+		phonePanel.add(htcBtn);
+		phonePanel.add(motoBtn);
+		
+		panel.add(headerPanel, BorderLayout.NORTH);
+		panel.add(phonePanel, BorderLayout.CENTER);
+		
+		panel2.add(panel, BorderLayout.NORTH);
+		panel2.add(pickOptionLbl, BorderLayout.CENTER);
+	}
+
+	private void addListeners() {
+		iPhoneBtn.addActionListener(this);
+		samsungBtn.addActionListener(this);
+		htcBtn.addActionListener(this);
+		motoBtn.addActionListener(this);
+	}
+
+	private void addPanelsToFrame() {
+		add(panel2);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
