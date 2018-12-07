f08a46aa8b756dce2389adace0bc357f512a2c8c
Alvin Madison <zo3101cn@my.century.edu>
2018-12-07 17:40:46 -0600

    Here's the corrected CategoryCollection.

--- src/edu/century/finalproject/shoppingcart/collections/CategoryCollection.java
+++ src/edu/century/finalproject/shoppingcart/collections/CategoryCollection.java
@@ -1,5 +1,33 @@
 package edu.century.finalproject.shoppingcart.collections;
 
+import edu.century.finalproject.shoppingcart.bean.Category;
+
 public class CategoryCollection {
+
+	ArrayBag<Category> categoryBag = new ArrayBag<>();
+
+	/**
+	 * @return the categoryBag
+	 */
+	public ArrayBag<Category> getCategoryBag() {
+		return categoryBag;
+	}
+
+	/**
+	 * @param categoryBag the categoryBag to set
+	 */
+	public void setCategoryBag(ArrayBag<Category> categoryBag) {
+		this.categoryBag = categoryBag;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "CategoryCollection [categoryBag=" + categoryBag + "]";
+	}
 
 }
