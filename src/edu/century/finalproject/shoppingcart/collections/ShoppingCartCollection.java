1dcf7630656d21be5d5b7746a52063a780516bf2
nn6388ic <nn6388ic@E1710-S1664892.century.edu>
2018-12-08 12:41:00 -0600

    Shopping cart collection added

--- src/edu/century/finalproject/shoppingcart/collections/ShoppingCartCollection.java
+++ src/edu/century/finalproject/shoppingcart/collections/ShoppingCartCollection.java
@@ -1,5 +1,33 @@
 package edu.century.finalproject.shoppingcart.collections;
 
-public class ShoppingCartCollection {
+import edu.century.finalproject.shoppingcart.bean.Catalog;
+
+/**
+ * @author Mo This will hold list of catelogue when add to cart is clicked
+ *
+ */
+public class ShoppingCartCollection
+{
+    private ArrayBag<Catalog> shoppingCartBag = new ArrayBag<>();
+
+    public ArrayBag<Catalog> getShoppingCartBag()
+    {
+        return shoppingCartBag;
+    }
+
+    public void setShoppingCartBag(ArrayBag<Catalog> shoppingCartList)
+    {
+        this.shoppingCartBag = shoppingCartList;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return "ShoppingCartCollection [shoppingCartBag=" + shoppingCartBag + "]";
+    }
 
 }
