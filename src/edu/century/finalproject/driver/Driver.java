0c4ddb3e09298cd7c60e0417a085d836158ddf97
arifc <arifc@DESKTOP-KUIAII6>
2018-12-10 17:25:59 -0600

    Make sure to add all the specifications for classes you did

--- src/edu/century/finalproject/driver/Driver.java
+++ src/edu/century/finalproject/driver/Driver.java
@@ -1,3 +1,17 @@
+/**
+ * Author: Arif Shahriar, Alvin Madison, Mohammed Safwat
+ * Instructor: Zakaria Baani 
+ * CourseID: 2082.01
+ * Due Date: 12/11/18
+ * Final Project: Mock Online Shopping Cart
+ * 
+ * We made a project on mock online shopping cart where user can get in 
+ * and buy electronic stuff. We had implemented add method where user
+ * can add all the materials they want to buy. There is remove method
+ * for deleting stuff from the cart. We had implemented a search method 
+ * where user can search for products that we sell.
+ */
+
 package edu.century.finalproject.driver;
 
 import java.io.BufferedReader;
